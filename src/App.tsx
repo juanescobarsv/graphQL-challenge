07dc30ecc02654afa6546dde8dd3aa8fc9528f04
Juan Escobar <juanescobar@ravn.co>
2025-05-26 17:59:13 -0600

    feat: added light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import "./styles/App.css";
 import "./styles/character-list.css";
 import "./styles/character-details.css";
 import "./styles/utils.css";
+import "./styles/colors.css";
+import "./styles/theme-toggle.css";
 
 function App() {
   const [selectedCharacterId, setSelectedCharacterId] = useState<string | null>(
@@ -17,10 +19,39 @@ function App() {
     setSelectedCharacterId(id);
   };
 
+  const [theme, setTheme] = useState<string>(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme || "light";
+  });
+
+  useEffect(() => {
+    document.body.className = theme === "dark" ? "dark-mode" : "";
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
     <div className="app-container">
       <header className="app-header">
         <h1>Ravn Rick and Morty Registry</h1>
+        <button
+          className="theme-toggle-button"
+          onClick={toggleTheme}
+          aria-label="Toggle theme"
+        >
+          {theme === "light" ? (
+            <span role="img" aria-label="Dark icon">
+              🌙
+            </span>
+          ) : (
+            <span role="img" aria-label="Light icon">
+              ☀️
+            </span>
+          )}
+        </button>
       </header>
 
       <div className="content-area">
